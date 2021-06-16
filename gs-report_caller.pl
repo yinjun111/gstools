ec3c9081e81f53fcb3ba400440999cc742d45153
Jun Yin <jun.yin@ferring.com>
2021-06-16 15:19:34 -0700

    bug fix for siglevel

--- gs-report_caller.pl
+++ gs-report_caller.pl
@@ -334,7 +334,7 @@ close IN;
 open(S1,">$scriptfile1") || die $!;
 foreach my $dbfile (sort keys %dbfiles) {
 	#both
-	print S1 "perl $gs_fisher -i $newsigfile -t matrix -c both -o $outputfolder/$dbfiles{$dbfile} -d $dbfile --top $topnum --siglevel $qcutoff -a ",$tx2ref{$tx}{"geneanno"},print_dev($dev,";");
+	print S1 "perl $gs_fisher -i $newsigfile -t matrix -c both -o $outputfolder/$dbfiles{$dbfile} -d $dbfile --top $topnum --qcutoff $qcutoff -a ",$tx2ref{$tx}{"geneanno"},print_dev($dev,";");
 	
 	if(@selcomparisons) {
 		print S1 "perl $gs_fisher_summary -i $outputfolder/$dbfiles{$dbfile} -o $outputfolder/$dbfiles{$dbfile}_summary --comparisons $comparisons --top $topnum --sortby $sortby",print_dev($dev,";\n");
